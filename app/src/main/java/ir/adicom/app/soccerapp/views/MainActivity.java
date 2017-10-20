87184cac992d101d6bfc26ed9b216b8e333b733f
Adicom <adicom.ir@gmail.com>
2017-10-20 21:30:12 +0330

    Init commit

---
+++ app/src/main/java/ir/adicom/app/soccerapp/views/MainActivity.java
@@ -0,0 +1,80 @@
+package ir.adicom.app.soccerapp.views;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import ir.adicom.app.soccerapp.R;
+import ir.adicom.app.soccerapp.models.Continent;
+import ir.adicom.app.soccerapp.presenters.IPresenter;
+import ir.adicom.app.soccerapp.presenters.Presenter;
+
+public class MainActivity extends AppCompatActivity implements IView {
+
+    private static final String LOG_TAG = "TAG";
+    private static final String TAG = "TAG";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        Realm.init(this);
+        Realm realm = Realm.getDefaultInstance();
+
+        showStatus("Perform basic Create/Read/Update/Delete (CRUD) operations...");
+
+        // All writes must be wrapped in a transaction to facilitate safe multi threading
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                // Add a continent
+                Continent continent = realm.createObject(Continent.class);
+                continent.setId(1);
+                continent.setName("Asia");
+
+            }
+        });
+
+        // Find the first continent (no query conditions) and read a field
+        final Continent continent = realm.where(Continent.class).findFirst();
+        showStatus(continent.getName());
+
+        // Update continent in a transaction
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                continent.setName("Africa");
+                showStatus(continent.getName());
+            }
+        });
+
+        // Delete all continents
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                realm.delete(Continent.class);
+            }
+        });
+
+        Typeface font = Typeface.createFromAsset(this.getAssets(), "fonts/splash.ttf");
+        TextView tvHeader = (TextView) findViewById(R.id.tvHeader);
+        if (tvHeader != null) tvHeader.setTypeface(font);
+        IPresenter presenter = new Presenter(this, this);
+    }
+
+    @Override
+    public void setGreeting(@NonNull String greeting) {
+        Log.e(LOG_TAG, greeting);
+    }
+
+    private void showStatus(String txt) {
+        Log.i(TAG, txt);
+    }
+}
